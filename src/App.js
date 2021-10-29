385821a09c4f95c85959d7215d9603325cc2c30b
Julia Barcelos <julaai@gmail.com>
2021-10-29 15:17:44 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './components/Header.js';
+import SolarSystem from './components/SolarSystem.js';
 
 class App extends React.Component {
   render() {
     return (
-    < Header/>
+      <>
+        < Header />
+        < SolarSystem />
+      </>
     );
   }
 }
