fb53abd0b9b118b460c4c34628db9b7bd9536360
Julia Barcelos <julaai@gmail.com>
2021-10-29 15:17:44 -0300

    Requisito 2

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+    <div data-testid="solar-system">
+    
+    </div>
+    );
+  }
+}
+
+export default SolarSystem;
+
